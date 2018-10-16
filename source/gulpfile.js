e62c3eba997a6f6ddc1537fc07fbeda4b7cb02ef
Kim Diprose <kim@triggerfish.com.au>
2018-10-16 14:51:52 +1000

    first commit

---
+++ source/gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp'); 
+
+var mjml = require('./source/index') 
+
+gulp.task('default', function() { 
+
+    gulp.src('./source/example.mjml') 
+
+    .pipe(mjml()) 
+
+    .pipe(gulp.dest('./prod/example.html ')) 
+
+});
+ 
+
+gulp.task('travel', function() { 
+
+  gulp.src('./source/travelNZ.mjml') 
+
+    .pipe(mjml()) 
+
+    .pipe(gulp.dest('./prod/travel/travelNZ.html ')) 
+
+});
